7476da1d501cf951e55999361ded5b9f6d455feb
LeMizoo <tovoniaina.rahendrison@gmail.com>
2025-11-24 13:19:09 +0300

    � Fix Vercel deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600
+  },
+  // Important pour Vercel
+  base: '/',
+  server: {
+    host: true
+  }
 })
